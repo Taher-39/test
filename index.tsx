103bd485df9f839eeb4580b646eeb6c0464da18c
Abu Taher <abut70243@gmail.com>
2024-03-16 22:12:23 +0600

    test commit

---
+++ index.tsx
@@ -0,0 +1,15 @@
+const Home: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">
+        Welcome to my Next.js Playlist
+      </h1>
+      <p className="text-lg text-red-500">
+        This is the first chapter where we set up a Next.js project with
+        Tailwind CSS.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
